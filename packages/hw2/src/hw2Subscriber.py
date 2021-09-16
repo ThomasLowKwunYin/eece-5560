1d5ec0f343736b5b243b63e4adf82bfefafe5310
ThomasLowKwunYin <thomas.low,kwun.yin@gmail.com>
2021-09-16 15:18:32 -0400

    patching_window_space

--- packages/hw2/src/hw2Subscriber.py
+++ packages/hw2/src/hw2Subscriber.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float32
+from std_msgs.msg import FLoat32
 
-class Listener:
-        def __init__(self):
-                rospy.Subscriber("chatter", Float32, self.callback)
+class Listener
+	def __init__(self):
+		rospy.Subscriber("chatter", Float32, self.callback)
+	
+	def callback(self,msg):
+		rospy.loginfo(f"{rospy.get_caller_id()}+I heard {msg.data}")
 
-        def callback(selfself,msg):
-            rospy.loginfo(f"{rospy.get_caller_id()}+I heard {msg.data}")
-
-if __name__ == '__main__':
-    rospy.init_node('listener', anonymous = True)
-    Listener()
-    rospy.spin()
+if __name__ = '__main__'
+	rospy.int_node('listener', anonymous = True)
+	Listener()
+	rospy.spin()
