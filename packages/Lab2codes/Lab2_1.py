2df9ac39e8592d36b116c08a16088d0dd8426201
ThomasLowKwunYin <31703069+ThomasLowKwunYin@users.noreply.github.com>
2021-11-12 00:30:46 -0500

    Update Lab2_1.py

--- packages/Lab2codes/Lab2_1.py
+++ packages/Lab2codes/Lab2_1.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python3
 
-import math
 import rospy
+import time
 from duckietown_msgs import FSMState, Twist2DStamped
 
 class Node:
 	def __init__(self):
-		
 		rospy.Subscriber('fsm_node/node', FSMState, self.talk)
 		self.pub = rospy.Publisher('car_cmd_switch_node/car_cmd', Twist2DStamped, queue_size=10)
     
-    self.moveCMD = Twist2DStamped()
-    self.moveCMD.v = 8.3
-    self.moveCMD.omega = 0
+		self.moveCMD = Twist2DStamped()
+		self.moveCMD.v = .41
+		self.moveCMD.omega = 0
+
+		self.stopCMD = Twist2DStamped()
+		self.stopCMD.v = 0
+		self.stopCMD.omega = 0
     
-    self.stopCMD = Twist2DStamped()
-    self.stopCMD.v = 0
-    self.stopCMD.omega = 0
-    
-    self.flag = False
+    		self.flag = False
     
 	def talk(self, msg)
-    
-    if msg.state == "LANE_FOLLOWING" and self.flag == False
-      self.flag = true
-      for i in range(102) #for 102 ticks
+		startTime = time.time()
+		
+		if msg.state == "LANE_FOLLOWING" and self.flag == False:
+			self.flag = True
+	      		for i in range(102): #for 102 ticks, apporximately 3 sec
+				curTIme = time.time()-startTIme
+				rospy.warninfo(f"{curTIme}: move")
+				self.pub.publish(self.moveCMD)
+			rospy.warninfo(f"{curTIme}: stop")
+			self.pub.publish(self.stopCMD)
+		else:
+			if msg.state != "LANE_FOLLOWING":
+				self.flag = False
+			self.pub.publish(self.stopCMD)
+				
       
 if __name__ == '__main__':
-  rospy.init_node('hw6Node', anonymous=True)
+	rospy.init_node('hw6Node', anonymous=True)
 	Node()
 	rospy.spin()
       
