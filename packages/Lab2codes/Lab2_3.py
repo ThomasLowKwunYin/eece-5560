57ebfafc0b72e9a83ebe1dedd157698541e63f1c
ThomasLowKwunYin <31703069+ThomasLowKwunYin@users.noreply.github.com>
2021-11-14 18:04:57 -0500

    Create Lab2_3.py

---
+++ packages/Lab2codes/Lab2_3.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+from duckietown_msgs import FSMState, Twist2DStamped
+
+class Node:
+	def __init__(self):
+		rospy.Subscriber('fsm_node/node', FSMState, self.talk)
+		self.pub = rospy.Publisher('car_cmd_switch_node/car_cmd', Twist2DStamped, queue_size=10)
+    
+		self.moveCMD = Twist2DStamped()
+		self.moveCMD.v = .41
+		self.moveCMD.omega = 0
+    
+    self.turnCMD = Twist2DStamped()
+		self.turnCMD.v = 0
+		self.turnCMD.omega = 2.1
+
+		self.stopCMD = Twist2DStamped()
+		self.stopCMD.v = 0
+		self.stopCMD.omega = 0
+    
+    		self.flag = False
+    
+	def talk(self, msg)
+		startTime = time.time()
+		
+		if msg.state == "LANE_FOLLOWING" and self.flag == False:
+			rospy.warninfo(f"{msg.state}")
+			self.flag = True
+	      		for i in range(102): #for 102 ticks, apporximately 3 sec
+				curTime = time.time()-startTIme
+				rospy.warninfo(f"{curTime}: move")
+				self.pub.publish(self.moveCMD)
+			rospy.warninfo(f"{curTime}: stop")
+			self.pub.publish(self.stopCMD)
+		else:
+			rospy.warninfo(f"{msg.state}")
+			if msg.state != "LANE_FOLLOWING":
+				self.flag = False
+			self.pub.publish(self.stopCMD)
+				
+      
+if __name__ == '__main__':
+	rospy.init_node('Lab2_3', anonymous=True)
+	Node()
+	rospy.spin()
