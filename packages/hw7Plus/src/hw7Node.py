d036455245a6af553214b8263d0311eaee6ba5c9
ThomasLowKwunYin <thomas.low.kwun.yin@gmail.com>
2021-11-22 20:26:46 -0500

    hw7

--- packages/hw7Plus/src/hw7Node.py
+++ packages/hw7Plus/src/hw7Node.py
@@ -6,37 +6,35 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 class node:
-    def __init__(self):
-        self.crop = rospy.Publisher("/image_cropped", Image, queue_size=10)
-        self.whiteMask = rospy.Publisher("/image_white", Image, queue_size=10)
-        self.yellowMask = rospy.Publisher("/image_yellow", Image, queue_size=10)
+	def __init__(self):
+		self.crop = rospy.Publisher("/image_cropped", Image, queue_size=10)
+		self.whiteMask = rospy.Publisher("/image_white", Image, queue_size=10)
+		self.yellowMask = rospy.Publisher("/image_yellow", Image, queue_size=10)
+		
+		rospy.Subscriber("/image", Image, self.talk)
+		
+		self.erosionKernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+		self.dilationKernel= cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+		self.bridge = CvBridge()
         
-        rospy.Subscriber("/image", Image, self.filter)
-        
-        self.erosionKernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-        self.dilationKernel= cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-        
-        self.bridge = CvBridge()
-        
-    def talk(self, msg):
-    	cv_img = self.bridge.imgmsg_to_cv2(msg,"bgr8")
-    	imgAxisY = len(cv_img)
-    	imgYHalf = int(imgAxisY/2)
-    	imgAxisX = len(cv_img[0])
-    	cropped = cv_img[int(imgAxisY/2):imgAxisY, 0:imgAxisX]
-    	
-    	whiteFilter  = cv2.inRange(hsv_img, (0,0,0),(180,25,255))
-        yellowFilter = cv2.inRange(hsv_img, (30,100,150),(40,255,255))
-        
-	crop = self.bridge.cv2_to_imgmsg(cropped,"bgr8")
-	whiteMask = self.bridge.cv2_to_imgmsg(whiteFilter, "mono8")
-	yellowMask = self.bridge.cv2_to_imgmsg(yellowFilter , "mono8")
-        
-	self.pub_cropped.publish(crop)
-	self.pub_white.publish(whiteMask)
-	self.pub_yellow.publish(yellowMask)
+	def talk(self, msg):
+		cv_img = self.bridge.imgmsg_to_cv2(msg,"bgr8")
+		imgAxisY = len(cv_img)
+		imgYHalf = int(imgAxisY/2)
+		imgAxisX = len(cv_img[0])
+		cropped = cv_img[int(imgAxisY/2):imgAxisY, 0:imgAxisX]
+		hsv_img = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
+		
+		whiteFilter  = cv2.inRange(hsv_img, (0,0,0), (180,25,255))
+		yellowFilter = cv2.inRange(hsv_img, (30,100,150), (40,255,255))
+		crop = self.bridge.cv2_to_imgmsg(cropped,"bgr8")
+		whiteMask = self.bridge.cv2_to_imgmsg(whiteFilter, "mono8")
+		yellowMask = self.bridge.cv2_to_imgmsg(yellowFilter , "mono8")
+		
+		self.whiteMask.publish(whiteMask)
+		self.yellowMask.publish(yellowMask)
     	
 if __name__ == "__main__":
-    rospy.init_node("node", anonymous=True)
-    ImageFilter()
-    rospy.spin()
+	rospy.init_node("node", anonymous=True)
+	node()
+	rospy.spin()
