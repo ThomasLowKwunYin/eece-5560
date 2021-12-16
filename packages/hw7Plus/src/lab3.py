d9d474ba4e8a6a578f183c0deaa14e2fee315d9a
ThomasLowKwunYin <thomas.low.kwun.yin@gmail.com>
2021-12-16 12:13:25 -0500

    lab3

--- packages/hw7Plus/src/lab3.py
+++ packages/hw7Plus/src/lab3.py
@@ -107,7 +107,7 @@ class Node:
 	def output_lines_white(self, original_image, lines):
 		output = np.copy(original_image)
 		if lines is not None:
-	    		for i in range(len(lines)):
+			for i in range(len(lines)):
 				l = lines[i][0]
 				cv2.line(output, (l[0],l[1]), (l[2],l[3]), (255,255,255), 2, cv2.LINE_AA)
 				cv2.circle(output, (l[0],l[1]), 2, (0,255,0))
@@ -116,7 +116,7 @@ class Node:
 	def output_lines_both(self, original_image, lines):
 		output = np.copy(original_image)
 		if lines is not None:
-	    		for i in range(len(lines)):
+			for i in range(len(lines)):
 				l = lines[i][0]
 				cv2.line(output, (l[0],l[1]), (l[2],l[3]), (255,255,0), 2, cv2.LINE_AA)
 				cv2.circle(output, (l[0],l[1]), 2, (0,255,0))
