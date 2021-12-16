a538c7d0c65d173a5df19a32b557fb152a72484e
ThomasLowKwunYin <thomas.low.kwun.yin@gmail.com>
2021-12-16 11:37:17 -0500

    hw9

--- packages/hw9Plus/src/hw9.py
+++ packages/hw9Plus/src/hw9.py
@@ -11,22 +11,24 @@ class node:
 		rospy.set_param("controller_ready", "true") 
 		rospy.set_param("graph_ready", "true") 
 
-		self.kp = .15
-		self.ki = 0
-		self.kd = .475
+		self.kp = 0.475
+		self.ki = 0.00005
+		self.kd = 0.085
 		self.error = 0
 		self.prevError = 0
 		self.errorDelta = 0
 		self.dt = 0.01
 		self.timeInit = rospy.get_time()
 	def talk(self, msg):
-		rospy.loginfo(msg.data)
+		rospy.logwarn(msg.data)
+		
 		time = rospy.get_time()
 		self.error += msg.data*(time-self.timeInit)
 		self.errorDelta = (self.error-self.prevError)/(time-self.timeInit)
 		self.timeInit = time
-		self.prevError = msg
-		output = self.kp*msg.daga+self.ki*self.error+self.kd*self.errorDelta
+		
+		self.prevError = msg.data
+		output = (self.kp*msg.data)+(self.ki*self.error)+(self.kd*self.errorDelta)
 		self.pub.publish(output)
 		
 if __name__ == "__main__":
