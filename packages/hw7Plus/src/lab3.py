56865e9fbb075e902bfcf8f748354471ba7dd779
ThomasLowKwunYin <thomas.low.kwun.yin@gmail.com>
2021-12-21 00:20:50 -0500

    lab3

--- packages/hw7Plus/src/lab3.py
+++ packages/hw7Plus/src/lab3.py
@@ -103,7 +103,7 @@ class Node:
 		#test output
 		testingUnit = whiteOverlay
 		rosTested = self.bridge.cv2_to_imgmsg(testingUnit,"mono8")
-		self.test.publish(rosTested)
+		self.testing.publish(rosTested)
 
 	def output_lines_white(self, original_image, lines):
 		output = np.copy(original_image)
