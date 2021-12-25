289dde8aa6b19865afeb68714aaed572b81a5ab2
ThomasLowKwunYin <thomas.low.kwun.yin@gmail.com>
2021-12-25 00:17:02 -0500

    hw10

--- packages/hw10/src/hw10.py
+++ packages/hw10/src/hw10.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 
 import rospy
-import example_service.srv
 import example_action_server.msg
 import actionlib
 
-class FibonacciAction(object):
-	_feedback = example_action_server.msg.FibonacciFeedback()
-	_result = example_action_server.msg.FibonacciResult()
+def HW10():
+	client = actionlib.SimpleActionClient('fibonacci' example_action_server.msg.FibonacciAction)
+	client.wait_for_server()
+	goal = example_action_server.msg.FibonacciGoal(order=10)
+	client.send_goal(goal)
+	client.wait_for_result()
+	return client.get_result()
 	
-	def _init_(self,name):
-		self._action_name=name
-		self._as = actionlib.SimpleActionServer(self._action_name, example_action_server.msg.FibonacciAction, execute_cb = self.execute_cb, auto_start = False)
-		self._as.start()
-		
-	def execute_cb(self,goal):
-		
 if __name__ == '__main__'
-	rospy.init_node('hw10')
-	server = FibonacciAction(rospy.get_name())
-	rospy.spin()
+	try:
+		rospy.init_node('hw10')
+		result = fibonacci_client()
+		print("Result:",','.join([str(n) for n in result.sequence]))
+	except rospy.ROSInterruptException:
+		print("program interrupted before completion", file = sys.stderr)
