7e4ec7342abd0a5ec47e1ce4874a43f61e0689cd
ThomasLowKwunYin <thomas.low.kwun.yin@gmail.com>
2021-12-26 15:40:57 -0500

    hw1

--- packages/hw10/src/hw10.py
+++ packages/hw10/src/hw10.py
@@ -2,20 +2,38 @@
 
 import rospy
 import example_action_server.msg
+import example_service.srv
 import actionlib
-
-def HW10():
-	client = actionlib.SimpleActionClient('fibonacci' example_action_server.msg.FibonacciAction)
-	client.wait_for_server()
-	goal = example_action_server.msg.FibonacciGoal(order=10)
-	client.send_goal(goal)
-	client.wait_for_result()
-	return client.get_result()
+class HW10:
+	def fiboserv(self, i):
+        rospy.wait_for_service('/calc_fibonacci')
+        try:
+            fib=rospy.ServiceProxy('/calc_fibonacci', Fibonacci)
+            start_time = rospy.get_time()
+            fib1 = fib(x)
+            rospy.loginfo(f"received at {rospy.get_time()-start_time}sec", )
+        except rospy.ServiceException as error:
+            rospy.loginfo(f"Service call failed: {%error}" )
+		
+		
+		
+	def fiboact(self, i):
+		client = actionlib.SimpleActionClient('fibonacci' example_action_server.msg.FibonacciAction)
+		client.wait_for_server()
+		goal = example_action_server.msg.FibonacciGoal(order=i)
+		startTime = rospy.get_time()
+		client.send_goal(goal)
+		rospy.loginfo(f"Request returned time: {rospy.get_time()-startTime}sec")
+		startTime = rospy.get_time()
+		rospy.loginfo(f"Answer returned time: {rospy.get_time()-startTime}sec")
+		client.wait_for_result()
+		
+		return client.get_result()
 	
 if __name__ == '__main__'
-	try:
-		rospy.init_node('hw10')
-		result = fibonacci_client()
-		print("Result:",','.join([str(n) for n in result.sequence]))
-	except rospy.ROSInterruptException:
-		print("program interrupted before completion", file = sys.stderr)
+	rospy.init_node('hw10')
+	node = HW10
+	node.fiboserv(3)
+	node.fiboact(3)
+	node.fiboserv(15)
+	node.fiboact(15)
